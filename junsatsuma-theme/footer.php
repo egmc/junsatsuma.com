486f25da75d64d0bc9767fc121d5bde3afcbbe8b
egmc <waltz4debby@eg2mix.com>
2020-03-01 14:39:10 +0900

    update footer link

--- junsatsuma-theme/footer.php
+++ junsatsuma-theme/footer.php
@@ -1,5 +1,5 @@
 			</div> <!-- end #container -->
-			
+
 			<footer role="contentinfo" id="footer">
 					<div id="inner-footer" class="clearfix container-fluid">
 			          <div id="widget-footer" class="clearfix row-fluid">
@@ -10,22 +10,22 @@
 			            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
 			            <?php endif; ?>
 			          </div>
-						
+
 						<nav class="clearfix">
 							<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 						</nav>
-				
-						<p class="attribution">&copy; junsatsuma.com&nbsp;since 2005</p>
-					
+
+						<p class="attribution">&copy; junsatsuma.com&nbsp;since 2005 / <a href="/privacy-policy">Privacy Policy</a></p>
+
 					</div> <!-- end #inner-footer -->
 			</footer> <!-- end footer -->
-		
-				
+
+
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 		<![endif]-->
-		
+
 		<?php wp_footer(); // js scripts are inserted using this function ?>
 		<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
