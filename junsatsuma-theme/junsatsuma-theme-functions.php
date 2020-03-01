f2cd3c42f892f516b150320d6a65d85f000da82f
egmc <waltz4debby@eg2mix.com>
2020-03-01 14:10:08 +0900

    fix title rule

--- junsatsuma-theme/junsatsuma-theme-functions.php
+++ junsatsuma-theme/junsatsuma-theme-functions.php
@@ -9,8 +9,16 @@ function getTitleImageURL() {
 	$title_text = the_title("", "", false);
 	$title_text = strtolower($title_text);
 	$title_text = str_replace(' ', '_', $title_text);
-	
-	$img_url = "/wp-content/themes/junsatsuma-theme/images/title_{$title_text}_250-60.png";
-	
+
+    $targe_path = "/images/title_{$title_text}_250-60.png";
+
+    $full_path = __DIR__ . $targe_path;
+
+    $img_url = "";
+
+    if (file_exists($full_path)) {
+        $img_url = "/wp-content/themes/junsatsuma-theme" . $targe_path;
+    }
+
 	return $img_url;
 }
--- junsatsuma-theme/page.php
+++ junsatsuma-theme/page.php
@@ -5,17 +5,17 @@ Template Name: Full Width Page
 ?>
 
 <?php get_header(); ?>
-			
+
 			<div id="content" class="clearfix row-fluid">
-			
+
 				<div id="main" class="span12 clearfix" role="main">
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
+
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
+
 						<header>
-							
+
 							<?php if (the_title("", "", false) == 'top') { ?>
 							<div id="slideshow">
 							<img src="/wp-content/themes/junsatsuma-theme/images/top_main_visual6_970-500.jpg" class="active" />
@@ -37,7 +37,7 @@ Template Name: Full Width Page
 							        : $('#slideshow IMG:first');
 
 							    $active.addClass('last-active');
-							        
+
 							    $next.css({opacity: 0.0})
 							        .addClass('active')
 							        .animate({opacity: 1.0}, 1000, function() {
@@ -48,29 +48,35 @@ Template Name: Full Width Page
 							});
 							</script>
 							<?php  } else { ?>
-							<div class="page-header"><h1><img src="<?php echo getTitleImageURL(); ?>" /></h1></div>
+							<div class="page-header">
+                                <h1>
+                                    <?php if (getTitleImageURL()) { ?>
+                                        <img src="<?php echo getTitleImageURL(); ?>" />
+                                    <?php  } else { the_title("", "", true); } ?>
+                                </h1>
+                            </div>
 						 	<?php } ?>
 						</header> <!-- end article header -->
-					
+
 						<section class="post_content">
 							<?php the_content(); ?>
-					
+
 						</section> <!-- end article section -->
-						
+
 						<footer>
-			
+
 							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","bonestheme") . ': ', ', ', '</span>'); ?></p>
-							
+
 						</footer> <!-- end article footer -->
-					
+
 					</article> <!-- end article -->
-					
+
 					<?php comments_template(); ?>
-					
+
 					<?php endwhile; ?>
-					
+
 					<?php else : ?>
-					
+
 					<article id="post-not-found">
 					    <header>
 					    	<h1><?php _e("Not Found", "bonestheme"); ?></h1>
@@ -81,13 +87,13 @@ Template Name: Full Width Page
 					    <footer>
 					    </footer>
 					</article>
-					
+
 					<?php endif; ?>
-			
+
 				</div> <!-- end #main -->
-    
+
 				<?php //get_sidebar(); // sidebar 1 ?>
-    
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
